4735cf9705d691d5cc02ba0286be50b756ebf92f
hariom-rcspl <hariom@rcspl.net>
2025-11-22 18:20:36 +0530

    login page improved

--- src/module/auth/pages/LoginPage.tsx
+++ src/module/auth/pages/LoginPage.tsx
@@ -36,53 +36,62 @@ export function LoginPage() {
 
     return (
         <div className="w-full min-h-screen p-10 grid items-center">
-            <Card className="max-w-md mx-auto w-full bg-white">
-                <CardHeader>
-                    <CardTitle>Login to your account</CardTitle>
-                    <CardDescription>
-                        Enter your email below to login to your account
-                    </CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <form onSubmit={formik.handleSubmit}>
-                        <div className="flex flex-col gap-3">
-                            <div>
-                                <Label htmlFor="email" className="mb-2">Email</Label>
-                                <Input
-                                    id="email"
-                                    type="email"
-                                    name="email"
-                                    value={formik.values.email}
-                                    onChange={formik.handleChange}
-                                    onBlur={formik.handleBlur}
-                                    placeholder="m@example.com"
-                                    required
-                                />
-                            </div>
-                            <div>
-                                <Label htmlFor="password" className="mb-2">Password</Label>
-                                <div className="relative">
+            <Card className="max-w-4xl mx-auto w-full p-0 overflow-hidden bg-white grid md:grid-cols-2">
+                <div className="flex flex-col justify-center py-7">
+                    <CardHeader>
+                        <CardTitle>Login to your account</CardTitle>
+                        <CardDescription>
+                            Enter your email below to login to your account
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <form onSubmit={formik.handleSubmit} className="mt-5">
+                            <div className="flex flex-col gap-3">
+                                <div>
+                                    <Label htmlFor="email" className="mb-2">Email</Label>
                                     <Input
-                                        id="password"
-                                        type={showPassword ? "text" : "password"}
-                                        placeholder="********"
-                                        required
-                                        value={formik.values.password}
+                                        id="email"
+                                        type="email"
+                                        name="email"
+                                        value={formik.values.email}
                                         onChange={formik.handleChange}
                                         onBlur={formik.handleBlur}
+                                        placeholder="m@example.com"
+                                        required
                                     />
-                                    <div
-                                        className="absolute top-1/2 right-2 -translate-y-1/2"
-                                        onClick={() => setShowPassword(!showPassword)}
-                                    >
-                                        {showPassword ? <Eye size={17} /> : <EyeClosed size={17} />}
+                                </div>
+                                <div>
+                                    <Label htmlFor="password" className="mb-2">Password</Label>
+                                    <div className="relative">
+                                        <Input
+                                            id="password"
+                                            type={showPassword ? "text" : "password"}
+                                            placeholder="********"
+                                            required
+                                            value={formik.values.password}
+                                            onChange={formik.handleChange}
+                                            onBlur={formik.handleBlur}
+                                        />
+                                        <div
+                                            className="absolute top-1/2 right-2 -translate-y-1/2"
+                                            onClick={() => setShowPassword(!showPassword)}
+                                        >
+                                            {showPassword ? <Eye size={17} /> : <EyeClosed size={17} />}
+                                        </div>
                                     </div>
                                 </div>
+                                <Button type="submit">Login</Button>
                             </div>
-                            <Button type="submit">Login</Button>
-                        </div>
-                    </form>
-                </CardContent>
+                        </form>
+                    </CardContent>
+                </div>
+                <div className="bg-muted relative hidden md:block h-96">
+                    <img
+                        src="/login.png"
+                        alt="Image"
+                        className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+                    />
+                </div>
             </Card>
         </div>
     )
